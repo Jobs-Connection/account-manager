fba1f118b9176db821d2d66d29ae5005c1490eb4
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-06-26 22:48:11 -0300

    feat: added database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', 
+      host: 'localhost', 
+      port: 5432, 
+      username: 'postgres', 
+      password: 'postgres',
+      database: 'account-manager', 
+      entities: []
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
--- src/modules/applicants/applicants.module.ts
+++ src/modules/applicants/applicants.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Applicant } from './applicant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Applicant])],
   controllers: [],
   providers: []
 })
